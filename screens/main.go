e2f1269f8cb1a032245df9f930c32cee861d005b
Zaeem Parker <iam@zp4rker.com>
2024-08-14 18:51:57 +1000

    Update main screen

--- screens/main.go
+++ screens/main.go
@@ -1,32 +1,55 @@
 package screens
 
 import (
+	"fauxos/commands"
+	"fauxos/filesystem"
 	"fauxos/styles"
 	"fmt"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"strings"
 )
 
 type MainScreen struct {
-	quitting      bool
-	input         textinput.Model
-	output        string
+	quitting bool
+	err      error
+
+	fs  map[string]filesystem.Node
+	cwd string
+
+	input  textinput.Model
+	output string
+
 	history       []string
 	historyCursor int
-	err           error
 }
 
 func MainScreenModel() MainScreen {
+	cwd := "/home/fox/"
 	input := textinput.New()
 	input.Focus()
-	input.Prompt = styles.Prompt1.Render("user@host ") + styles.Prompt2.Render("~/some/path ")
+	input.Prompt = getPrompt(cwd)
+	input.TextStyle = styles.Ansi[7]
 
 	return MainScreen{
-		input:         input,
-		output:        "",
+		err: nil,
+
+		fs: map[string]filesystem.Node{
+			"bin": filesystem.Directory{Name: "bin"},
+			"home": filesystem.Directory{Name: "home", Files: map[string]filesystem.Node{
+				"fox": filesystem.Directory{Name: "fox", Files: map[string]filesystem.Node{
+					"readme.txt": filesystem.File{Name: "readme.txt", Contents: []byte("this is a test file")},
+					"work":       filesystem.Directory{Name: "work"},
+				}},
+			}},
+		},
+		cwd: cwd,
+
+		input:  input,
+		output: "",
+
 		history:       make([]string, 0),
 		historyCursor: -1,
-		err:           nil,
 	}
 }
 
@@ -72,6 +95,8 @@ func (m MainScreen) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 
+	m.input.Prompt = getPrompt(m.cwd)
+
 	m.input, cmd = m.input.Update(msg)
 	return m, cmd
 }
@@ -82,23 +107,29 @@ func (m MainScreen) View() string {
 		finalOutput += m.input.View()
 	}
 
-	return fmt.Sprintf("Welcome to FoxOS!\n%s", finalOutput)
+	splash := styles.Ansi[8].Render("*********************")
+	splash += "\n" + styles.Ansi[11].Copy().Bold(true).Render("FoxOS") + " v0.0.1-alpha"
+	splash += "\nby " + styles.Ansi[4].Copy().Bold(true).Render("zp4rker")
+	splash += "\n" + styles.Ansi[8].Render("*********************")
+	return fmt.Sprintf("%s\n%s", strings.TrimSpace(splash), finalOutput)
 }
 
 func (m MainScreen) handleCommand(command string) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
+	m.input.Cursor.Blink = true
+	m.output += m.input.View() + "\n"
+
 	switch command {
 	case "quit", "exit", "logout":
-		m.input.Cursor.Blink = true
-		m.output += m.input.View() + "\n"
 		m.output += "Quitting FoxOS...\n"
 		m.quitting = true
 		cmd = tea.Quit
+	case "lf", "list-files":
+		m.output += commands.ListFiles(strings.Trim(m.cwd, "/"), m.fs)
+
 	default:
-		m.input.Cursor.Blink = true
-		m.output += m.input.View() + "\n"
-		m.output += "Could not find command '" + command + "'!\n"
+		m.output += "Could not find command '" + command + "'\n"
 	}
 
 	m.history = append(m.history, command)
@@ -106,4 +137,8 @@ func (m MainScreen) handleCommand(command string) (tea.Model, tea.Cmd) {
 	m.input.Reset()
 
 	return m, cmd
+}
+
+func getPrompt(path string) string {
+	return styles.Prompt1.Render("user@host ") + styles.Prompt2.Render(path) + " "
 }
